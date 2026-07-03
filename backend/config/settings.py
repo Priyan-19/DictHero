54f9096158bb5833b97638515a853f39e05e52b8
Priyan-19 <priyan190406@gmail.com>
2026-07-03 18:24:16 +0530

    add

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -93,6 +93,7 @@ else:
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
     'DEFAULT_PARSER_CLASSES':   ['rest_framework.parsers.JSONParser'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
 
 # Gemini
